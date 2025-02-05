73345d893ebdcae3b20934e234f2601ca86e8dba
xaviSapalomera <151662884+xaviSapalomera@users.noreply.github.com>
2025-02-05 20:24:13 +0100

    Update model_ajax_article.php

--- model/model_ajax_article.php
+++ model/model_ajax_article.php
@@ -6,7 +6,7 @@ $articleModel = new Article();
 $articles = $articleModel->mostrarTotsArticles();
 
 if ($articles === false || empty($articles)) {
-    echo json_encode(["error" => "No se pudieron obtener los artículos"]);
+    echo json_encode(["error" => "No se a pugut obtindre el article"]);
 } else {
     echo json_encode($articles, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 }
