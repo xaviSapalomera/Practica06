0e069e9d73c25916a9a16b07b6d1ab61eb888a7a
xaviSapalomera <151662884+xaviSapalomera@users.noreply.github.com>
2025-02-05 20:23:18 +0100

    Update controlador_generador_qr.php

--- controller/controlador_generador_qr.php
+++ controller/controlador_generador_qr.php
@@ -7,20 +7,20 @@ use BaconQrCode\Renderer\Image\SvgImageBackEnd;
 use BaconQrCode\Writer;
 
 try {
-    // Obtindra el ID del artícle   desde la URL
+    // Obtindra el ID del artícle desde la URL
     $article_id = isset($_GET['id']) ? $_GET['id'] : '1';
     $url = "http://localhost/Practica06-main/article.php?id=" . $article_id;
 
-    // Crear el renderer con los namespaces correctos
+    
     $renderer = new ImageRenderer(
-        new RendererStyle\RendererStyle(400), // Intentaste llamar a RendererStyle incorrectamente
+        new RendererStyle\RendererStyle(400), 
         new SvgImageBackEnd()
     );
 
     $writer = new Writer($renderer);
     $qrCode = $writer->writeString($url);
 
-    // Mostrar el código QR en formato SVG
+    // Mostrar el QR
     header('Content-Type: image/svg+xml');
     echo $qrCode;
 } catch (Exception $e) {
